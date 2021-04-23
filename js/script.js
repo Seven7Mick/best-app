c98872a922f53d0fa73543dbd2072c4f8f186254
Dmitry Shmelkov <seven_mick@mail.ru>
2021-04-23 18:13:37 +0700

    responsive

--- js/script.js
+++ js/script.js
@@ -2,15 +2,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const buttonScrollToDown = document.getElementById('scroll-down');
   const buttonScrollToUp = document.getElementById('scroll-up');
-
+  // Scroll Down
   buttonScrollToDown.addEventListener('click', () => {
     const footerPage = document.querySelector('.footer');
     footerPage.scrollIntoView({behavior: "smooth"});
   })
-
+  // Scroll Up
   buttonScrollToUp.addEventListener('click', () => {
     const header = document.querySelector('.header');
     header.scrollIntoView({behavior: "smooth"});
   })
+  // Menu
+  const header = document.querySelector('.header');
+  const burger = header.querySelector('.header__menu-burger');
+  const body = document.querySelector('body');
+
+
+  const toggleMenu = () => {
+    header.classList.toggle('header_menu_open');
+    body.classList.toggle('body_overflow_hidden')
+    
+  };
+
+  burger.addEventListener ('click', toggleMenu)
 
 })
