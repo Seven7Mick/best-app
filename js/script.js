c58278cedf010a796474978ffdfc2dd425e5c4d2
Dmitry Shmelkov <seven_mick@mail.ru>
2021-04-24 11:52:32 +0700

    scroll for safari

--- js/script.js
+++ js/script.js
@@ -2,18 +2,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const buttonScrollToDown = document.getElementById('scroll-down');
   const buttonScrollToUp = document.getElementById('scroll-up');
-  // Scroll Down
-  buttonScrollToDown.addEventListener('click', () => {
-    const footerPage = document.querySelector('.footer');
-    footerPage.scrollIntoView({behavior: "smooth"});
-  })
-  // Scroll Up
-  buttonScrollToUp.addEventListener('click', () => {
-    const header = document.querySelector('.header');
-    header.scrollIntoView({behavior: "smooth"});
-  })
-  // Menu
+
+  const isSafari = navigator.userAgent.match(/ipad|iphone/i) || navigator.userAgent.includes("Mac");
+
   const header = document.querySelector('.header');
+  const footerPage = document.querySelector('.footer');
+
+  if (isSafari) {
+    $('html, body').animate({
+      scrollTop: footerPage.offset().bottom
+    }, 1000)
+  } else {
+    // Scroll Down
+    buttonScrollToDown.addEventListener('click', () => {
+      footerPage.scrollIntoView({ behavior: "smooth" });
+    })
+  }
+
+  if (isSafari) {
+    $('html, body').animate({
+      scrollTop: footerPage.offset().top
+    }, 1000)
+  } else {
+    // Scroll Up
+    buttonScrollToUp.addEventListener('click', () => {
+      header.scrollIntoView({ behavior: "smooth" });
+    })
+  }
+
+  // Menu
   const burger = header.querySelector('.header__menu-burger');
   const body = document.querySelector('body');
 
@@ -21,9 +38,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const toggleMenu = () => {
     header.classList.toggle('header_menu_open');
     body.classList.toggle('body_overflow_hidden')
-    
+
   };
 
-  burger.addEventListener ('click', toggleMenu)
+  burger.addEventListener('click', toggleMenu)
 
 })
